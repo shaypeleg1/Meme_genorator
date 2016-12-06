f438965940392e2d194ffbf0436474a587e6b7d1
Mark Fridman <marky.fridman@gmail.com>
2016-12-06 16:57:28 +0200

    init and more functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 'use strict';
 console.log('Hello Meme');
+
+var gMemes = [
+    {
+        id: 2,
+        url: 'img2.jpg',
+        keywords: ['animal', 'history', 'skeptic'],
+        rating: 0
+    },
+    {
+        id: 3,
+        url: 'img3.jpg',
+        keys: ['animal'],
+        rating: 0
+    }
+];
+
+function init() {
+    renderGallery(gMemes);
+}
+
+function renderGallery(memes) {
+
+}
+
+function addNewMeme(imgUrl, keywords) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
 function addUserMem() {
     console.log('addUserMem button');
 }

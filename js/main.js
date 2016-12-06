d1aa14a84062031881280dbc2b2e8f5b856e358b
Mark Fridman <marky.fridman@gmail.com>
2016-12-06 20:23:56 +0200

    canvas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 console.log('Hello Meme');
-
+var gCtx;
 var gMemes = [
     {
         id: 0,
@@ -11,19 +11,35 @@ var gMemes = [
     {
         id: 1,
         url: 'img/memes/meme_2.jpg',
-        keys: ['history'],
+        keywords: ['history'],
         rating: 0
     }
 ];
 
 $(document).ready(function(){
     renderGallery(gMemes);
+    initCanvas();
+
+    $(".hexagon").click(function(node){
+        var strUrl = node.currentTarget.style.backgroundImage;
+        strUrl = strUrl.substr(5,strUrl.length-7);
+        console.log(node);
+        
+        drawImgOnCanvas(strUrl);
+    });
+
 });
+
+function initCanvas() {
+    var canvas;
+    canvas = document.getElementById('canvas');
+    gCtx = canvas.getContext('2d');
+}
 
 function renderGallery(memes) {
     memes.forEach(function(memObj) {
         var hex = '<div id="'+memObj.id+'" class="hexagon"'+
-                ' style="background-image: url('+memObj.url+')">'+
+                ' style="background-image: url('+memObj.url+')">' +
                     '<div class="hexTop"></div>'+
                     '<div class="hexBottom"></div>'+
                 '</div>';
@@ -40,8 +56,9 @@ function searchForKeyword(string) {
     var matchedMemes = [];
     gMemes.forEach(function(meme) {
         meme.keywords.forEach(function(key) {
+            
             if(key === string) {
-                matchedMemes.push(meme);
+                matchedMemes.push(meme);  
             }
         });
     });
@@ -52,4 +69,15 @@ function searchForKeyword(string) {
 
 function addUserMem() {
     console.log('addUserMem button');
+}
+
+function drawImgOnCanvas(imgUrl) {
+    console.log('img pressed!' , imgUrl);
+    var img = new Image();
+    img.src = imgUrl;
+
+    gCtx.drawImage(img, 0, 0, 400, 300);
+    gCtx.font = "60px 'Segoe UI'";
+    gCtx.fillText("print on Canvas", 50, 300);
+
 }

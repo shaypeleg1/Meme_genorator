e46f07f057342f5ca64c7e72f24bb9df5b8418dc
Mark Fridman <marky.fridman@gmail.com>
2016-12-08 11:08:01 +0200

    drawtext func update

--- js/text-tools.js
+++ js/text-tools.js
@@ -6,21 +6,26 @@ function drawTextOnCanvas(gTextObj) {
     var elImg = gCurrElImg;
 
     gCtx.drawImage(elImg, 0, 0, elImg.width, elImg.height);
-    gCtx.font = "60px 'Segoe UI'";
 
     gCtx.textAlign = gTextObj['topText'].align;
+    gCtx.font = gTextObj['topText'].size+"px 'Segoe UI'";
     gCtx.fillText(gTextObj['topText'].text, elImg.width/2, elImg.height*0.2); 
 
     gCtx.textAlign = gTextObj['bottomText'].align;
+    gCtx.font = gTextObj['bottomText'].size+"px 'Segoe UI'";
     gCtx.fillText(gTextObj['bottomText'].text, elImg.width/2, elImg.height*0.85); 
 
 }
 
-function buttonIntersec(prop, value, direction) {
+function buttonIntersec(direction, prop, value) {
     switch(prop) {
         case 'text':
             gState[direction].text = value;
             drawTextOnCanvas(gState);
+        break;
+
+        case 'align':
+            gState[direction].align = value;
         break;
     }
 }
